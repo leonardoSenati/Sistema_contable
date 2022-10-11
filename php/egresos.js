2fa2e071f4b93244d86ef4a36c8b538b86841868
RonalSTimoteo <86899692+RonalSTimoteo@users.noreply.github.com>
2022-10-10 20:56:52 -0500

    Update egresos.js

--- php/egresos.js
+++ php/egresos.js
@@ -1,8 +1,20 @@
+/*
+Evento click que se dispara cuando se presiona el link de tipo button para abrir el panel egreso.
+ */
 $(document).on('click','#egre',function(){
     recuperar();
     document.getElementById("panel").innerText="Panel egreso";
+    document.getElementById("titulo_panel").innerText="Panel egresos";
 });
 
+/**
+ * Esta funcion envia datos a traves del metodo GET por medio ajax al archivo bus_tipo_egre.php
+ * método $.ajax
+ * URL - para la petición.
+ * type - especifica que será una petición GET.
+ * data - la información a enviar.
+ *  la var cabecera que contiene el formulario para obtener los datos del cliente,proyecto,descripción, tipo Egreso, Monto y fecha.
+ */
 function recuperar(){
     var cabecera=`
     <div class="main">
@@ -62,8 +74,7 @@ function recuperar(){
             </table> 
         </div>
     </div>`;
-    $('#cabecera').html(cabecera);//
-    
+    $('#cabecera').html(cabecera);
     $.ajax({
         url:'../php/bus_tipo_egre.php',
         type:'GET',
@@ -78,7 +89,6 @@ function recuperar(){
             $('#egreso').html(registros);
         }  
     });
-    
     $.ajax({
         url:'../php/bus_cli.php',
         type:'GET',
@@ -93,17 +103,27 @@ function recuperar(){
             $('#datalistOptions').html(registro);
         }  
     });
-
     fecha_actual("fecha");
 }
 
-
+/**
+ * Evento keyup para detectar la pulsación de una tecla en el input con id ruc.
+ * ruc - con el metodo getElementById seleccionamos el elemento por medio del id ruc y capturamos su valor.
+ * método $.ajax 
+ * URL - ruta del archivo bus_cli.php para la petición.
+ * type - especifica que será una petición GET
+ * success - Establece una función a ejecutar si la petición a sido satisfactoria.
+ * lista_actividad - variable que almacenar al objeto 
+ * fila - varible de tipo array - almacena los elemento del objeto.
+ * registros - variable de estructura html donde a la etiqueta option en su valor le colocamos los datos que tiene los respectivos elementos.
+ * datalistOptions - id de la etiqueta datalist - colocando los valores de la var registros en el datalist.
+ * nombre_proyectos - funcion que va enviar como parametro a la variable ruc.
+ */
 $(document).on('keyup','#ruc',function(){
-    var ruc=document.getElementById('ruc').value; 
-    
+    var ruc=document.getElementById('ruc').value;
     $.ajax({
         url:'../php/bus_cli.php',
-        type:'GET',//tipo get
+        type:'GET',
         success:function(data){
             var lista_actividad=JSON.parse(data);
             var registros='';
@@ -119,25 +139,32 @@ $(document).on('keyup','#ruc',function(){
     nombre_proyectos(ruc);
 });
 
-
-
+/**
+ * funcion que recibe como parametro el ruc del cliente.
+ * método $.ajax 
+ * URL - ruta del archivo proyecto.php para la petición.
+ * type - especifica que será una petición POST
+ * success - Establece una función a ejecutar si la petición a sido satisfactoria.
+ * lista_actividad - variable que almacenar al objeto 
+ * fila - variable de tipo array - almacena los elementos del objeto.
+ * registro - variable de estructura html donde a la etiqueta option en su valor le colocamos los datos que tiene los respectivos elementos.
+ * proyect - id de la etiqueta datalist - colocando los valores de la var registros en el datalist.
+ */
 function nombre_proyectos(ruc_cliente){
 
     $.ajax({
-        url:'../php/proyecto.php',
-        data:{ruc_cliente},
+        url:'../php/proyecto.php' ,
+        data:{ruc_cliente},//envia la variable
         type: 'POST',
         success: function(data){
                 var lista_actividad=JSON.parse(data);
                 var registro='';
-                
                 lista_actividad.forEach(fila=>{
                     registro +=`
                     <option value="${fila.id_proyecto}">${fila.n_proyecto}</option>
                     `;  
                            
                 });
-                
             $('#proyects').html(registro);
         }
     });
